cae9fb99562ff5e05198dfa52f688e46e08cf587
Francesco Cappio Borlino <francesco.cappioborlino@tuta.io>
2023-05-26 11:12:22 +0200

    Added script folder for scripts to run batches of methods

---
+++ scripts/run_DomainNet_DG.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+src_domain="$1"
+tgt_domain="$2"
+
+echo "Running with source $src_domain and target $tgt_domain"
+
+
+#### CE - ResNet
+ 
+python train.py --dataset DomainNet_DG --source "$src_domain" --target "$tgt_domain" --network resnet101 --model CE --only_eval
+
+#### CE - Vit
+
+python train.py --dataset DomainNet_DG --source "$src_domain" --target "$tgt_domain" --network vit --model CE --only_eval
+
+#### CutMix - SimCLR - SupCLR - SupCSI
+
+for model in cutmix simclr supclr CSI supCSI; do python train.py --dataset DomainNet_DG --source "$src_domain" --target "$tgt_domain" --network resnet101 --model "$model"  --only_eval --checkpoint_path pretrained_models/"$model"_r101.pth; done
+
+#### Mahalanobis on CE 
+
+python train.py --dataset DomainNet_DG --source "$src_domain" --target "$tgt_domain" --network resnet101 --model CE --only_eval --evaluator mahalanobis 
+
+#### KNN on CE 
+
+python train.py --dataset DomainNet_DG --source "$src_domain" --target "$tgt_domain" --network resnet101 --model CE --only_eval --evaluator knn_distance
+
+#### MCM 
+
+python train.py --dataset DomainNet_DG --source "$src_domain" --target "$tgt_domain" --network resnet101 --model clip --only_eval --evaluator MCM
