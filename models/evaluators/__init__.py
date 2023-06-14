e994f3652afdab893edd1272eec47f1920646717
Francesco Cappio Borlino <francesco.cappioborlino@tuta.io>
2023-06-14 11:40:42 +0200

    Added react evaluator

--- models/evaluators/__init__.py
+++ models/evaluators/__init__.py
@@ -1,5 +1,5 @@
 from .distance import prototypes_distance_evaluator, knn_distance_evaluator, knn_ood_evaluator
-from .discriminative import MSP_evaluator, ODIN_evaluator, energy_evaluator, gradnorm_evaluator
+from .discriminative import MSP_evaluator, ODIN_evaluator, energy_evaluator, gradnorm_evaluator, react_evaluator
 from .mahalanobis import mahalanobis_evaluator
 from .gram import gram_evaluator
 from .linear_probe import linear_probe_evaluator
